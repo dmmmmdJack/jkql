f1d1ef1d52a7d356e4759427cc84d9116b7bbaf7
阿西吧 <83806077+LYC-axiba@users.noreply.github.com>
2023-05-12 19:40:34 +0800

    Update jd_wxShopGift.js

--- jd_wxShopGift.js
+++ jd_wxShopGift.js
@@ -1,3 +1,5 @@
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://172.17.0.1:8899";
 /*
 活动名称：店铺礼包特效 · 超级无线
 活动链接：https://lzkj-isv.isvjcloud.com/wxShopGift/activity?activityId=<活动id>
