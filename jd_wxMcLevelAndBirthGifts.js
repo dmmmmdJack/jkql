eb0910d58683210214f5250a56439eb3b0591cc4
阿西吧 <83806077+LYC-axiba@users.noreply.github.com>
2023-05-12 19:35:49 +0800

    Update jd_wxMcLevelAndBirthGifts.js

--- jd_wxMcLevelAndBirthGifts.js
+++ jd_wxMcLevelAndBirthGifts.js
@@ -1,3 +1,5 @@
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://172.17.0.1:8899";
 /*
 活动名称：生日礼包/会员等级礼包 · 超级无线
 活动链接：https://cjhy-isv.isvjcloud.com/mc/wxMcLevelAndBirthGifts/activity?activityId=<活动id>
