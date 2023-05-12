3d2f930340985f32a134c61da6eff33e8af46e72
阿西吧 <83806077+LYC-axiba@users.noreply.github.com>
2023-05-12 19:32:08 +0800

    Update jd_wxCollectionActivity.js

--- jd_wxCollectionActivity.js
+++ jd_wxCollectionActivity.js
@@ -1,3 +1,5 @@
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://172.17.0.1:8899";
 /*
 活动名称：加购有礼 · 超级无线
 活动链接：https://lzkj-isv.isvjcloud.com/wxCollectionActivity/activity?activityId=<活动id>
