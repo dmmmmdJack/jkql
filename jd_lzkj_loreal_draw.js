f9227f5ffd3038b04721078e96a18ad0896692a5
阿西吧 <83806077+LYC-axiba@users.noreply.github.com>
2023-05-12 18:49:35 +0800

    Update jd_lzkj_loreal_draw.js

--- jd_lzkj_loreal_draw.js
+++ jd_lzkj_loreal_draw.js
@@ -1,3 +1,5 @@
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://172.17.0.1:8899";
 /**
 活动名称：店铺抽奖（超级无线欧莱雅）
 活动链接：https://lzkj-isv.isvjcloud.com/prod/cc/interactsaas/index?activityType=<10020/10021/10026/10080>&templateId=<模板id>&activityId=<活动id>&nodeId=<nodeid>&prd=cjwx
